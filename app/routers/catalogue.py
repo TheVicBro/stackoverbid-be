1498a5ac43c58d650e4c8e53adf82858fd118b40
Ricky Nguyen <ricky.nguyen981@gmail.com>
2026-02-23 21:07:21 -0500

    Add item search filter

--- app/routers/catalogue.py
+++ app/routers/catalogue.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
 from app.models import models
 from app.schemas import schemas
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/catalogue",
@@ -11,6 +12,17 @@ router = APIRouter(
 )
 
 @router.get("/items", response_model=List[schemas.Item])
-def get_items(db: Session = Depends(get_db)):
-    # Skeleton: Return all items
-    return db.query(models.Item).all()
+def get_items(
+    db: Session = Depends(get_db), 
+    keyword: Optional[str] = None, 
+):
+    query = db.query(models.Item)
+    
+    query = query.filter(models.Item.status == "active")
+    
+    if keyword:
+        pattern = f"%{q}%"
+        query = query.filter(models.Item.title.ilike(pattern)) 
+    
+    return query.all()
+        
