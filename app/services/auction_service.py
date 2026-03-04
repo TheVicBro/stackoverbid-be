36be9ce0fc72dd2a40edb95039fdbc2188f6cd6b
zuker-joshua <90483754+zuker-joshua@users.noreply.github.com>
2026-03-03 21:55:38 -0500

    Fixed some labelling issues

--- app/services/auction_service.py
+++ app/services/auction_service.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import Session  # type: ignore[import]
 from fastapi import HTTPException
+from sqlalchemy import func
 
 from app.daos import bid_dao, item_dao
 from app.schemas import schemas
+from app.models import models
 
 
 def create_item(db: Session, item_in: schemas.ItemCreate, seller_id: int) -> schemas.Item:
@@ -19,9 +21,9 @@ def place_bid(db: Session, item_id: int, user_id: int, bid_in: schemas.BidCreate
     if item.status != "active":
         raise HTTPException(status_code=400, detail="The auction has ended.")
 
-    highest_bid = bid_dao.get_highest_bid_for_item(db, item_id)
+    highestBid = (db.query(func.max(models.Bid.amount)).filter(models.Bid.item_id == item_id).scalar())
 
-    min_required = highest_bid if highest_bid is not None else item.starting_price
+    min_required = highestBid if highestBid is not None else item.starting_price
 
     if bid_in.amount <= min_required:
         raise HTTPException(status_code=400,detail = f"Bid must be greater than {min_required}")
